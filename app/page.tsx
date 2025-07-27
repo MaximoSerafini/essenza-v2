704d41becad8cf9816cb6e0fd97d0d37a8d9b255
Maximo Serafini <111703978+MaximoSerafini@users.noreply.github.com>
2025-07-27 17:28:45 -0300

    jean lowe

--- app/page.tsx
+++ app/page.tsx
@@ -124,24 +124,6 @@ const perfumes: Perfume[] = [
     rating: 4.9,
   },
   {
-    id: 24,
-    marca: "Maison Alhambra",
-    nombre: "Jean Lowe Fraiche 100ml EDP",
-    imagen: "https://i.imgur.com/xa8yGJW.png",
-    precio: 26000,
-    notas: {
-      salida: ["Pétalos de rosa", "Caramelo"],
-      corazon: ["Jazmín", "Cacao", "Madera de agar (oud)"],
-      fondo: ["Cuero", "Ámbar", "Vainilla"],
-    },
-    genero: "Hombre",
-    fragancia_referencia: "Nouveau Monde de Louis Vuitton",
-    descripcion:
-      "Exótica, sofisticada y envolvente. Jean Lowe Fraiche captura el espíritu aventurero combinando dulzura, especias y sensualidad profunda.",
-    sinDescuento: true,
-    rating: 4.8,
-  },
-  {
     id: 35,
     marca: "Lattafa Perfumes",
     nombre: "Mayar 100ml EDP",
