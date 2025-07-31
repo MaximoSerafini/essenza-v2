edc8663a5d8641421655696853df9ff21ca56a1c
Maximo Serafini <111703978+MaximoSerafini@users.noreply.github.com>
2025-07-31 15:04:19 -0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -209,24 +209,6 @@ const perfumes: Perfume[] = [
     sinDescuento: false,
   },
   {
-    id: 46,
-    marca: "Maison Alhambra",
-    nombre: "Jean Lowe Inmortal 30ml EDP",
-    imagen: "https://i.imgur.com/qJVdGgU.png",
-    precio: 15000,
-    notas: {
-      salida: ["Pétalos de rosa", "Caramelo"],
-      corazon: ["Jazmín", "Cacao", "Madera de agar (oud)"],
-      fondo: ["Cuero", "Ámbar", "Vainilla"],
-    },
-    genero: "Hombre",
-    fragancia_referencia: "Nouveau Monde de Louis Vuitton",
-    descripcion:
-      "Exótica, sofisticada y envolvente. Jean Lowe Fraiche captura el espíritu aventurero combinando dulzura, especias y sensualidad profunda.",
-    sinDescuento: true,
-    rating: 4.8,
-  },
-  {
     id: 31,
     marca: "Maison Alhambra",
     nombre: "Céleste 100ml EDP",
